fa1e796c5f76d749a8c87be7a201257c33bd15f3
CarmineD8 <carmine.recchiuto@dibris.unige.it>
2021-11-09 11:50:40 +0000

    update

--- client.cpp
+++ client.cpp
@@ -1,15 +1,16 @@
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
-#include <actionlib_tutorials/FibonacciAction.h>
+#include <my_actions/FibonacciAction.h>
 
 int main (int argc, char **argv)
 {
+	
   ros::init(argc, argv, "test_fibonacci");
 
   // create the action client
   // true causes the client to spin its own thread
-  actionlib::SimpleActionClient<actionlib_tutorials::FibonacciAction> ac("fibonacci", true);
+  actionlib::SimpleActionClient<my_actions::FibonacciAction> ac("fibonacci", true);
 
   ROS_INFO("Waiting for action server to start.");
   // wait for the action server to start
@@ -17,7 +18,7 @@ int main (int argc, char **argv)
 
   ROS_INFO("Action server started, sending goal.");
   // send a goal to the action
-  actionlib_tutorials::FibonacciGoal goal;
+  my_actions::FibonacciGoal goal;
   goal.order = 20;
   ac.sendGoal(goal);
 
@@ -28,9 +29,19 @@ int main (int argc, char **argv)
   {
     actionlib::SimpleClientGoalState state = ac.getState();
     ROS_INFO("Action finished: %s",state.toString().c_str());
+    my_actions::FibonacciResultConstPtr seq= ac.getResult();
+    for (int i = 0; i < goal.order; i++){
+		std::cout << seq->sequence[i] << " ";
+	}
+	std::cout << std::endl;
+    
   }
-  else
+  else{
     ROS_INFO("Action did not finish before the time out.");
+    ac.cancelGoal(); 
+    ROS_INFO("Goal has been cancelled");
+   }
+
 
   //exit
   return 0;
